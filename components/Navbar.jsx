f204af797eb92eb81f790a9b9fa3234b36ccb2dd
hasnat5 <hasnatferdiananda5@gmail.com>
2023-02-04 15:55:25 +0700

    add navbar and footer

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <div>ini navbar</div>
+        <div>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/about">About Us</Link>
+                </li>
+                <li>
+                    <Link href="/contact">Contact</Link>
+                </li>
+            </ul>
+        </div>
     )
 }
 
