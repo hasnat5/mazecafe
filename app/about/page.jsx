9608355ffdd50c06d867e37ca613b519f812ad03
hasnat5 <hasnatferdiananda5@gmail.com>
2023-02-04 14:57:41 +0700

    mazecafe

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1 className='text-3xl text-green-300'>About Maze</h1>
+        </div>
+    )
+}
+
+export default About
